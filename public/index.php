1c2f1905a41bb6bc0b90576f5b92a214d6ce14bd
Babak-Chalacki <babak.chalaki82@gmail.com>
2025-09-01 16:08:22 +0330

    [ADD] routing

--- public/index.php
+++ public/index.php
@@ -1,2 +1,11 @@
 <?php
-echo "Hello, My First Framework!";
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Veloxia\Core\Router;
+
+$router = new Router();
+
+require __DIR__ . '/../routes/web.php';
+
+$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
